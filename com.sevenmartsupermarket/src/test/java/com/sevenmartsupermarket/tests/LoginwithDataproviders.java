a2a5f0aade97b8ae659dea24be2cf5a932690e8d
USER <USER@DESKTOP-LQV4VHB>
2024-03-15 07:26:51 +0530

    new testcase

---
+++ com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/tests/LoginwithDataproviders.java
@@ -0,0 +1,32 @@
+package com.sevenmartsupermarket.tests;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.sevenmartsupermarket.dataproviders.DataProviders;
+import com.sevenmartsupermarket.pages.HomePage;
+import com.sevenmartsupermarket.pages.LoginPage;
+import com.sevenmartsupermarket.base.Base;
+
+;
+
+public class LoginwithDataproviders extends Base {
+	
+	LoginPage loginpage;
+	HomePage homepage;
+	
+	@Test(dataProvider ="sevenmartsupermarket", dataProviderClass = DataProviders.class )
+	
+	public void verifyLoginWithDifferntAdminUsers(String userName, String Password) {
+		loginpage = new LoginPage(driver);
+		homepage = new HomePage(driver);
+		loginpage.login(userName, Password);
+		 Assert.assertTrue(homepage.homepageisDisplayed(), "Login failed for username: " + userName);
+		
+		
+		
+	}	
+	
+	
+
+}
