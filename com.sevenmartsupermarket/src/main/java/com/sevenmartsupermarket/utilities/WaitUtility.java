6be624d14942eef281978ee4de46ef8aa6550042
USER <USER@DESKTOP-LQV4VHB>
2024-03-26 11:22:32 +0530

    commit wait

--- com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/utilities/WaitUtility.java
+++ com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/utilities/WaitUtility.java
@@ -1,11 +1,14 @@
 package com.sevenmartsupermarket.utilities;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtility {
@@ -21,17 +24,27 @@ public class WaitUtility {
 	}
 
 	// Method to wait for an element's visibility by its XPath
-	/*public void waitForElementVisibilityByXPath(WebDriver driver, String xpath, long time) {
+	public void waitForElementVisibilityByXPath(WebDriver driver, String xpath, long time) {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(time));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
 
-	}*/
+	}
 
 	
-	public void waitForElementVisibilityByXPath(WebDriver driver, WebElement element, long time) {
+	public void waitForElementVisibilityByXPath1(WebDriver driver, WebElement element, long time) {
 		
 		wait = new WebDriverWait(driver, Duration.ofSeconds(time));
 		wait.until(ExpectedConditions.visibilityOf(element));
 
+	}
+	
+	
+	public void waitForElementWithFluentWait(WebDriver driver, long timeoutInSeconds, long pollingEveryInMiliSec, String xpath) {
+	    Wait<WebDriver> wait = new FluentWait<>(driver)
+	            .withTimeout(Duration.ofSeconds(timeoutInSeconds)) // Use the timeoutInSeconds parameter
+	            .pollingEvery(Duration.ofMillis(pollingEveryInMiliSec)) // Use the pollingEveryInMiliSec parameter for polling frequency
+	            .ignoring(NoSuchElementException.class);
+
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
 	}
 }
